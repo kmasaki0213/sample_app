032169f56786b351192ac8f049a401a4f2d29cc1
kmasaki0213 <102206918+kmasaki0213@users.noreply.github.com>
2025-02-10 05:28:54 +0000

    Render-build ReFix

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,5 +4,5 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:drop db:create db:migrate
+bundle exec rails db:migrate
 
